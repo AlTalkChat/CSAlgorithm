37b7d00f9dcb23682fb638dc907aa04cc31e3d7d
안려환 <arh951019@gmail.com>
2023-07-27 23:37:59 +0900

    docs:완탐 피로도완료

---
+++ 안려환/0726_완전탐색/PGMRS_피로도.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PGMRS_피로도 {
+    public static void main(String[] args) {
+        int k = 80;
+        int [][] dungeons = {{80,80},{50,40},{30,10}};
+        boolean [] visited = new boolean[dungeons.length];
+        List<int []> list = new ArrayList<>();
+        for (int [] a: dungeons) {
+            list.add(a);
+        }
+        status status = new status();
+        status.status(k,list,visited,0);
+        System.out.println(status.result);
+    }
+}
+
+class status{
+    public int result = 0;
+    public void status(int k , List<int[]> dungeons,boolean [] visited,int count){
+        if(k<=0){
+            System.out.println("작아졌니");
+            result = Math.max(result,count);
+            return;
+        }
+        if(!check(k,dungeons,visited)){
+            System.out.println("가능한게 더이상 없대");
+            System.out.println("visited: "+ Arrays.toString(visited));
+            System.out.println("현재 count : "+count);
+            result = Math.max(result,count);
+            System.out.println("현재 result : "+result);
+            return;
+        }
+        System.out.println("count : "+count);
+        for (int i = 0; i < dungeons.size(); i++) {
+            if(dungeons.get(i)[0] <= k && !visited[i]){
+                visited[i] = true;
+                status(k-dungeons.get(i)[1],dungeons,visited,count+1);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    public boolean check(int k, List<int[]> list,boolean [] visited){
+        for (int i = 0; i < list.size(); i++) {
+            if(!visited[i] && list.get(i)[0]<=k){
+                System.out.println("k: "+k+" list[0]: "+list.get(i)[0]+" list[1]: "+list.get(i)[1]+" 가능");
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
