23bb43ef58b974cb3a206f69ad0eb3ad94f9f379
안려환 <arh951019@gmail.com>
2023-08-22 01:16:01 +0900

    code: 2개이하비트 in progress

--- 안려환/RyeoAlgo/src/PGMRS_2개이하로다른비트.java
+++ 안려환/RyeoAlgo/src/PGMRS_2개이하로다른비트.java
@@ -3,6 +3,9 @@ public class PGMRS_2개이하로다른비트 {
     public static void main(String[] args) {
         String a;
         long b;
+        // 보니까 이게 앞에 하나를 추가하거나 아니면 맨 뒤에 추가하거나 하는 식드로 하면 될 것 같은디
+        // 10 100 11 둘 중에서 2 이하로 다르면서 작은건 11 아닌감
+        // 그리고 111 에서 1111 아니면 1000 인데 그냥 1111 이 답 아닌감 1011
         for (int i = 0; i < numbers.length; i++) {
             a = Long.toBinaryString(numbers[i]);
             // 1000 이면 1001 이 답이고
