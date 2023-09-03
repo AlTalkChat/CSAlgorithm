e2e557644836a06620b3891acc7c2211fb687f78
안려환 <arh951019@gmail.com>
2023-09-03 23:45:45 +0900

    code:add hamburger

---
+++ 안려환/RyeoAlgo/src/PGMRS_햄버거만들기.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class PGMRS_햄버거만들기 {
+    private static int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < ; i++) {
+
+        }
+
+    }
+    private static void makeBurger(){
+        Stack<Integer> stack = new Stack<>();
+                        
+    }
+}
