d2cdcb83c35f2bafd7f25e13290aa522a6d64684
안려환 <arh951019@gmail.com>
2023-10-14 23:23:16 +0900

    code: add algo study

---
+++ 안려환/RyeoAlgo/src/PGMRS_달리기경주.java
@@ -0,0 +1,9 @@
+public class PGMRS_달리기경주 {
+    private static String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+    private static String[] callings = {"kai", "kai", "mine", "mine"};
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(players[1]);
+        System.out.println(sb.reverse());
+    }
+}
