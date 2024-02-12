1020a98b5070fd920ec92df6f5a78d971d52332d
안려환 <arh951019@gmail.com>
2024-02-12 18:16:58 +0900

    code: 성격유형검사 추가

--- 안려환/RyeoAlgo/src/PGMRS_햄버거만들기.java
+++ 안려환/RyeoAlgo/src/PGMRS_햄버거만들기.java
@@ -2,7 +2,6 @@ import java.util.Stack;
 
 public class PGMRS_햄버거만들기 {
     private static int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
-
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         int answer = 0;
